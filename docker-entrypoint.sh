c560ffa8bcd3eca3dd41a09308cf93e0bb30e991
Valeria Silva <silvvavaleria@gmail>
2021-11-30 07:12:10 -0300

    Update image docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 python -m spacy download pt
-
+#sudo pip install pyspark==3.0.0
+#sudo pip install delta-spark
 ### Spark monitor
 #pip install jupyterlab-sparkmonitor # no supported for spark 3.1.1
-#pip install -r requirements.txt
+sudo pip install -r requirements.txt
 #pip install sparkmonitor
 #
 #sudo -E pip install jupyter_contrib_nbextensions
@@ -23,7 +24,7 @@ if [ "$1" = 'jupyter' ]; then
                                --NotebookApp.token='' \
                                --NotebookApp.password=''
 fi
-
+delta-spark 1.0.0-
 if [ "$1" = 'bash' ]; then
  /bin/bash
 fi
